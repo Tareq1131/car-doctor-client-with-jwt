77d9dfb5e638b72148ddc5779e78e73c2c0d7a89
Tareq1131 <tareqislam076@gmail.com>
2024-02-26 02:12:04 +0600

    created about section

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom"
+import Footer from "../pages/Shared/Footer/Footer"
+import Navbar from "../pages/Shared/NavBar/Navbar"
 
 
 function Main() {
   return (
     <div>
+      <Navbar/>
       <Outlet/>
+      <Footer/>
     </div>
   )
 }
